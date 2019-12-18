ba886f57eaa1b1a2ea1bc0130cecb46a1d2c9232
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-12-18 05:14:04 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,44 @@
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+      puts 'this is a valid move'
+    return true
+  else
+   return false
+  end
+end
+
+def input_to_index(user_input)
+   user_input.to_i - 1
+end
+
+def move(board, index_i, char = "X")
+  board[index_i] = char
+end
+
+def turn(board) 
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  valid_move?(board, index)
+  if valid_move?(board,index)
+    puts "valid move"
+    move(board, index)
+    display_board(board)
+   else
+    puts "try again"
+    turn(board)
+  end 
+end 
