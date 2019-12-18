e807eaf7ec7dfd7ab242ab147ea77a62be393152
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-12-18 17:25:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   valid_move?(board, index)
-  if valid_move?(board,index)
+  if true
     puts "valid move"
     move(board, index, token = "X")
     display_board(board)
